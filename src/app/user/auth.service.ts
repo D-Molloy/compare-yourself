5a02d8260596de70d393e40ee34bb374d8ef44ca
Denis Molloy <denis.molloy@gmail.com>
2018-09-13 13:55:05 -0700

    first commit

---
+++ src/app/user/auth.service.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs/Subject';
+
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
+
+import { User } from './user.model';
+
+const POOL_DATA = {
+  UserPoolId:"us-west-2_6pSU5BrcQ",
+  ClientId:"1t3agfhnq1on04c3bdrekd14na"
+};
+
+const userPool = new CognitoUserPool(POOL_DATA);
+
+
+@Injectable()
+export class AuthService {
+  authIsLoading = new BehaviorSubject<boolean>(false);
+  authDidFail = new BehaviorSubject<boolean>(false);
+  authStatusChanged = new Subject<boolean>();
+  constructor(private router: Router) {}
+  signUp(username: string, email: string, password: string): void {
+    this.authIsLoading.next(true);
+    const user: User = {
+      username: username,
+      email: email,
+      password: password
+    };
+    const emailAttribute = {
+      Name: 'email',
+      Value: user.email
+    };
+    return;
+  }
+  confirmUser(username: string, code: string) {
+    this.authIsLoading.next(true);
+    const userData = {
+      Username: username,
+    };
+  }
+  signIn(username: string, password: string): void {
+    this.authIsLoading.next(true);
+    const authData = {
+      Username: username,
+      Password: password
+    };
+    this.authStatusChanged.next(true);
+    return;
+  }
+  getAuthenticatedUser() {
+  }
+  logout() {
+    this.authStatusChanged.next(false);
+  }
+  isAuthenticated(): Observable<boolean> {
+    const user = this.getAuthenticatedUser();
+    const obs = Observable.create((observer) => {
+      if (!user) {
+        observer.next(false);
+      } else {
+        observer.next(false);
+      }
+      observer.complete();
+    });
+    return obs;
+  }
+  initAuth() {
+    this.isAuthenticated().subscribe(
+      (auth) => this.authStatusChanged.next(auth)
+    );
+  }
+}
