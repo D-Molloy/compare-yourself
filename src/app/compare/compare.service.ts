a077b502045ae6471b6ca627126279a725985937
Denis Molloy <denis.molloy@gmail.com>
2018-09-13 15:55:19 -0700

    finished app

--- src/app/compare/compare.service.ts
+++ src/app/compare/compare.service.ts
@@ -23,8 +23,13 @@ export class CompareService {
     this.dataIsLoading.next(true);
     this.dataEdited.next(false);
     this.userData = data;
-      this.http.post('https://API_ID.execute-api.REGION.amazonaws.com/dev/', data, {
-        headers: new Headers({'Authorization': 'XX'})
+    this.authService.getAuthenticatedUser().getSession((err, session)=>{
+      if(err){
+        return;
+      };
+
+      this.http.post('https://u8o0terzik.execute-api.us-west-2.amazonaws.com/dev/compare-yourself', data, {
+        headers: new Headers({'Authorization': session.getIdToken().getJwtToken() })
       })
         .subscribe(
           (result) => {
@@ -38,17 +43,21 @@ export class CompareService {
             this.dataEdited.next(false);
           }
         );
+
+
+    });
   }
   onRetrieveData(all = true) {
     this.dataLoaded.next(null);
     this.dataLoadFailed.next(false);
-      let queryParam = '';
+    this.authService.getAuthenticatedUser().getSession((err, session)=>{
+      const queryParam = '?accessToken=' + session.getAccessToken().getJwtToken();
       let urlParam = 'all';
       if (!all) {
         urlParam = 'single';
       }
-      this.http.get('https://API_ID.execute-api.REGION.amazonaws.com/dev/' + urlParam + queryParam, {
-        headers: new Headers({'Authorization': 'XXX'})
+      this.http.get('https://u8o0terzik.execute-api.us-west-2.amazonaws.com/dev/compare-yourself/' + urlParam + queryParam, {
+        headers: new Headers({'Authorization': session.getIdToken().getJwtToken()})
       })
         .map(
           (response: Response) => response.json()
@@ -72,11 +81,14 @@ export class CompareService {
             this.dataLoaded.next(null);
           }
         );
+    })
+
   }
   onDeleteData() {
     this.dataLoadFailed.next(false);
-      this.http.delete('https://API_ID.execute-api.REGION.amazonaws.com/dev/', {
-        headers: new Headers({'Authorization': 'XXX'})
+    this.authService.getAuthenticatedUser().getSession((err, session)=>{
+      this.http.delete('https://u8o0terzik.execute-api.us-west-2.amazonaws.com/dev/compare-yourself/?accessToken=XXX', {
+        headers: new Headers({'Authorization': session.getIdToken().getJwtToken()})
       })
         .subscribe(
           (data) => {
@@ -84,5 +96,7 @@ export class CompareService {
           },
           (error) => this.dataLoadFailed.next(true)
         );
+    });
+      
   }
 }
